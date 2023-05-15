c8a5a38bfd288b35701aba75ed7dd088dd058f18
goidescugeorgiana <goidescugeorgiana19@stud.ase.ro>
2023-05-16 02:10:56 +0300

    new5 frontend init

--- js/components/InsertPage.js
+++ js/components/InsertPage.js
@@ -21,26 +21,26 @@ export default function InsertPage() {
 	return (
 		<section className="bg-white dark:bg-gray-900">
 			<div className="container px-6 py-10 mx-auto">
-				<h1 className="w-[500px] mx-auto text-center text-6xl">Fun facts app</h1>
-				<p className="w-[1000px] mx-auto text-center mt-4 text-3xl">This is an app that showcases fun facts</p>
+				<h1 className="w-[500px] mx-auto text-center text-5xl">Muzeele din București</h1>
+				<p className="w-[1000px] mx-auto text-center mt-4 text-2xl">Ghidul tău turistic ce îți prezintă informații despre cele mai fascinante muzee din capitala României. Acum e mult mai simplu să vizitezi fiind informat!</p>
 
 				<form>
 					<div className="mb-6">
-						<label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fact title</label>
+						<label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Denumire Muzeu</label>
 						<input type="text" id="title"
 						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						       placeholder="name@flowbite.com" required/>
+						       placeholder="ex: Muzeul Național de Artă" required/>
 					</div>
 					<div className="mb-6">
 						<label htmlFor="description"
-						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fact description</label>
+						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Informații</label>
 						<textarea id="description"
 						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						       required/>
 					</div>
-					<button type="submit"
+					<button type="trimte"
 					        onClick={ insertRecord }
-					        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+					        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Trimite
 					</button>
 				</form>
 			</div>
