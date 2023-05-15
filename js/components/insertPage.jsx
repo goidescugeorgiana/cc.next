fd441137b61050b2ccfbac0dba1a6ad81b082506
goidescugeorgiana <goidescugeorgiana19@stud.ase.ro>
2023-05-16 01:56:44 +0300

    new4 frontend commit

--- js/components/insertPage.jsx
+++ js/components/insertPage.jsx
@@ -1,4 +1,4 @@
-//  js/components/InsertPage.jsx
+// js/components/InsertPage.jsx
 export default function InsertPage() {
 	const insertRecord = (event) => {
 		event.preventDefault();
@@ -21,19 +21,19 @@ export default function InsertPage() {
 	return (
 		<section className="bg-white dark:bg-gray-900">
 			<div className="container px-6 py-10 mx-auto">
-				<h1 className="w-[500px] mx-auto text-center text-5xl">Muzeele din București</h1>
-				<p className="w-[1000px] mx-auto text-center mt-4 text-2xl">Ghidul tău turistic ce îți prezintă informații despre cele mai fascinante muzee din capitala României. Acum e mult mai simplu să vizitezi fiind informat!</p>
+				<h1 className="w-[500px] mx-auto text-center text-6xl">Fun facts app</h1>
+				<p className="w-[1000px] mx-auto text-center mt-4 text-3xl">This is an app that showcases fun facts</p>
 
 				<form>
 					<div className="mb-6">
-						<label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numele muzeului</label>
+						<label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fact title</label>
 						<input type="text" id="title"
 						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						       placeholder="ex: Muzeul Național de Artă" required/>
+						       placeholder="name@flowbite.com" required/>
 					</div>
 					<div className="mb-6">
 						<label htmlFor="description"
-						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Informații muzeu</label>
+						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fact description</label>
 						<textarea id="description"
 						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						       required/>
