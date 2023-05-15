f3c839c5e04c708e0000ad8f47e977a3ce4d45cd
goidescugeorgiana <goidescugeorgiana19@stud.ase.ro>
2023-05-16 02:03:40 +0300

    new5 frontend init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './js/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './js/components/**/*.{js,ts,jsx,tsx}'
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -17,3 +19,4 @@ module.exports = {
   },
   plugins: [],
 }
+
