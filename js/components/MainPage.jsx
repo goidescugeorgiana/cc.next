bc6a323dbe4b60d6e8025a584ed33e073a8157c6
goidescugeorgiana <goidescugeorgiana19@stud.ase.ro>
2023-05-16 02:55:25 +0300

    new6 frontend commit

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -61,7 +61,16 @@ export default function MainPage() {
 						</div>
 					))}
 				</div>
-			</div>
+				
+				
+				<div class="flex justify-center mt-4">
+    <a href="http://localhost:3000/insert"
+            class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">
+        Adaugă alt muzeu
+    </a>
+</div>			
+				</div>
 		</section>
 	)
+
 }
