e9cbc22b25e2cced987568da9dda03bba1f03a22
goidescugeorgiana <goidescugeorgiana19@stud.ase.ro>
2023-05-16 03:10:18 +0300

    new7 frontend init

--- js/components/InsertPage.js
+++ js/components/InsertPage.js
@@ -41,12 +41,12 @@ export default function InsertPage() {
 			
 					<div class="flex justify-between items-center mt-4">
   
-  
+
   <button type="trimte"
           onClick={ insertRecord }
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Trimite
   </button>
-  <a href="http://localhost:3000"
+  <a href="https://cc-next-git-main-goidescugeorgiana.vercel.app"
      class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">
     Înapoi
   </a>
